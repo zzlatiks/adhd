a23aee66f1b3642dabd76782418e5f389db5ac3a
root <root@vm964950.u242867.kvm.ubuntu-22.04-amd64-r1>
2025-09-18 16:37:11 +0000

    Fix: обновил tailwind.config.js safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   safelist: [
-    { pattern: /(bg|text|border)-(red|blue|green|yellow|purple|pink|gray)-(100|200|300|400|500|600|700|800|900)/ }
+    { pattern: /(bg|text|border)-(red|blue|green|yellow|purple|pink|gray)-(100|200|300|400|500|600|700|800|900)/ },
+    'absolute',
+    'relative',
+    'top-full',
+    'right-0',
+    'left-0',
+    'z-10',
+    'z-20',
+    'z-50',
+    'bg-white',
+    'border',
+    'border-gray-200',
+    'rounded-lg',
+    'shadow-lg',
+    'min-w-[120px]'
   ],
   theme: {
     extend: {},
