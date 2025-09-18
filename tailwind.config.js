01807e2d854e31def3080e851b1460af0681c1a9
root <root@vm964950.u242867.kvm.ubuntu-22.04-amd64-r1>
2025-09-18 15:48:27 +0000

    Fix Tailwind build configs: vite, postcss, tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}'
+  ],
+  safelist: [
+    { pattern: /(bg|text|border)-(red|blue|green|yellow|purple|pink|gray)-(100|200|300|400|500|600|700|800|900)/ }
+  ],
   theme: {
     extend: {},
   },
